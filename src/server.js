36c41ae111146b2587cbd0d23b8b1dcabb947101
Moyinoluwalogo <m.oladipsgmail.com>
2022-07-29 20:15:45 +0100

    Completed the project, creating, configuring and initializing the bot, included configurations like sending a message to a specific chat, scheduling a message to be sent and configuring sutomated responses.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,48 @@
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+// Here we setup our server connecting our whatsapp web chatbot server to whatsapp through QR code
+const { Client, LocalAuth } = require("whatsapp-web.js")
+const qrcode = require("qrcode-terminal") // ->To display the actual QR code in the terminal and not the textual form
 
-client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
+// To ensure our session continues affter we restart the server
+const client = new Client({
+	authStrategy: new LocalAuth(),
+})
+
+// Starting a new session i.e connecting to whatsapp for the first time
+// QR code is generated on the terminal to be scanned on your whatsapp account.
+client.on("qr", (qr) => {
+	qrcode.generate(qr, { small: true })
+})
+// After the QR code and the whatsapp account is linked this is displayed to notify success
+client.on("ready", () => {
+	console.log("Client is ready!")
+
+	// To send a message to a specific chat.
+	client.getChats().then((chats) => {
+		// Finding the chat we intend to send a message to
+		const botChat = chats.find((chat) => chat.name === "Whatsapp Automation bot")
+		// We send  message to the group specifying the chat's id and the message
+		client.sendMessage(
+			botChat.id._serialized,
+			"Hello, This is an Automated message"
+		)
+        console.log('Message sent') 
+
+		// To schedule a message
+		console.log('Message scheduled')
+		setTimeout(() => {
+		    client.sendMessage(botChat.id._serialized, "From Your baby's code, Tell your proud bum bum i said Hi")
+		}, 20000); // -> 20000 here indicates After 20 seconds
+	})
+})
+
+/**Configuring Automated responses
+ * The client listens to a message event, contanined n this event is the body which contains the actual text.
+ * Responses can then be configured for the text recieved with an if condition. */ 
+
+client.on('message', message => {
+	if(message.body === 'Hey') {
+		message.reply(`Hello, Welcome to this Services, How can i help you`);
+	}
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
-client.initialize();
-
-// Download Chrome
+client.initialize()
